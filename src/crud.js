99234783711258003f9b6e9fbc2b0756131329a7
Peter MX <36844547+blakbox23@users.noreply.github.com>
2021-08-25 09:38:20 +0300

    avoid reloading browser after deleting an item

--- src/crud.js
+++ src/crud.js
@@ -4,10 +4,7 @@ const ul = document.querySelector('ul');
 const taskDescription = document.querySelector('#txt-input');
 const deleteAllBtn = document.querySelector('.clear-all');
 
-const addToLocalStorage = () => {
-  const storage = JSON.stringify(tasks);
-  localStorage.setItem('lstore', storage);
-};
+
 
 const getFromLocalStorage = () => {
   const storage = JSON.parse(localStorage.getItem('lstore')) || [];
@@ -16,90 +13,96 @@ const getFromLocalStorage = () => {
 
 const tasks = getFromLocalStorage();
 
+const addToLocalStorage = () => {
+  const storage = JSON.stringify(tasks);
+  localStorage.setItem('lstore', storage);
+};
+
+
 const createUi = (task) => {
- 
-    const li = document.createElement('li');
-    li.setAttribute('id', task.index);
-    const lDiv = document.createElement('div');
-    lDiv.classList.add('ldiv');
-    const lDivSpan = document.createElement('span');
-    lDivSpan.classList.add('ldiv-spn');
-    const rDiv = document.createElement('div');
-    rDiv.classList.add('rdiv');
-    const pTask = document.createElement('span');
-    const checkbox = document.createElement('input');
-    checkbox.type = 'checkbox';
-    checkbox.name = 'task-box';
-    checkbox.value = task;
-    checkbox.classList.add('chkbx');
+  const li = document.createElement('li');
+  li.setAttribute('id', task.index);
+  const lDiv = document.createElement('div');
+  lDiv.classList.add('ldiv');
+  const lDivSpan = document.createElement('span');
+  lDivSpan.classList.add('ldiv-spn');
+  const rDiv = document.createElement('div');
+  rDiv.classList.add('rdiv');
+  const pTask = document.createElement('span');
+  const checkbox = document.createElement('input');
+  checkbox.type = 'checkbox';
+  checkbox.name = 'task-box';
+  checkbox.value = task;
+  checkbox.classList.add('chkbx');
 
-    li.appendChild(lDiv);
+  li.appendChild(lDiv);
 
-    lDiv.appendChild(lDivSpan);
-    lDiv.appendChild(pTask);
-    lDivSpan.appendChild(checkbox);
+  lDiv.appendChild(lDivSpan);
+  lDiv.appendChild(pTask);
+  lDivSpan.appendChild(checkbox);
 
-    li.appendChild(rDiv);
+  li.appendChild(rDiv);
 
+  rDiv.innerHTML = '<i class="fas fa-bars"></i>';
+
+  ul.appendChild(li);
+
+  li.addEventListener('mouseover', () => {
+    rDiv.innerHTML = '<i class="fas fa-trash-alt"></i>';
+  });
+
+  li.addEventListener('mouseout', () => {
     rDiv.innerHTML = '<i class="fas fa-bars"></i>';
+  });
 
-    ul.appendChild(li);
+  pTask.addEventListener('click', () => {
+    editTasks(pTask, task);
+  });
 
-    li.addEventListener('mouseover', () => {
-      rDiv.innerHTML = '<i class="fas fa-trash-alt"></i>';
-    });
+  rDiv.addEventListener('click', () => {
+    deleteTask(rDiv.parentElement);
+  });
 
-    li.addEventListener('mouseout', () => {
-      rDiv.innerHTML = '<i class="fas fa-bars"></i>';
-    });
+  if (task.completed === true) {
+    pTask.innerText = task.description;
+    pTask.classList.add('cancel');
+    pTask.previousSibling.children[0].checked = true;
+  } else {
+    pTask.innerText = task.description;
+  }
 
-    pTask.addEventListener('click', () => {
-      editTasks(pTask, task);
-    });
-
-    rDiv.addEventListener('click', () => {
-      deleteTask(rDiv.parentElement.id);
-    });
-
-    if (task.completed === true) {
-      pTask.innerText = task.description;
-      pTask.classList.add('cancel');
-      pTask.previousSibling.children[0].checked = true;
+  checkbox.addEventListener('change', (e) => {
+    if (e.target.checked) {
+      taskCompleted(task);
+      e.target.parentElement.nextSibling.classList.add('cancel');
+      addToLocalStorage();
     } else {
-      pTask.innerText = task.description;
+      taskNotComplete(task);
+      e.target.parentElement.nextSibling.classList.remove('cancel');
+      addToLocalStorage();
     }
-
-    checkbox.addEventListener('change', (e) => {
-      if (e.target.checked) {
-        taskCompleted(task);
-        e.target.parentElement.nextSibling.classList.add('cancel');
-        addToLocalStorage();
-      } else {
-        taskNotComplete(task);
-        e.target.parentElement.nextSibling.classList.remove('cancel');
-        addToLocalStorage();
-      }
-    });
-
+  });
 };
 
 const ui = () => {
   tasks.forEach((task) => {
-    createUi(task)
-  })
-}
+    createUi(task);
+  });
+};
 
 ui();
 
 const addToTasks = (e) => {
   e.preventDefault();
-  let item = {
+  const item = {
     description: taskDescription.value,
     completed: false,
     index: tasks.length,
-  }
+  };
   tasks.push(item);
   createUi(item);
+ 
+
   addToLocalStorage();
   taskDescription.value = '';
 };
@@ -122,7 +125,7 @@ const deleteTask = (listId) => {
   const index = listId;
   tasks.splice(index, 1);
   addToLocalStorage();
-  window.location.reload();
+  listId.remove();
 };
 
 const deleteAllCompleted = () => {
--- src/index.js
+++ src/index.js
@@ -1,13 +1,7 @@
 import './style.css';
 
-import {
-  addToLocalStorage, tasks, editTasks, addToTasks, deleteTask,
-} from './crud.js';
+import { addToTasks } from './crud.js';
 
 const form = document.querySelector('#task-form');
 
 form.addEventListener('submit', addToTasks);
-
-
-
-
